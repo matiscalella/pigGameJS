4b41e6df2ad7abd5b03999ab744a7222d436d85d
Matias Scalella <matias.scalella@live.com>
2018-07-31 17:02:45 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,10 @@
+/*
+Reglas del juego:
+
+- Se juega por turnos, 2 jugadores.
+- En cada turno, el jugador hace girar el dado tantas veces como quiera. Cada resultado del dado se agrega a su puntaje ROUND.
+- PERO, si el jugador tira un 1 todo su puntaje ROUND se pierde y es el turno del proximo jugador.
+- El jugador puede elegir la opcion 'Hold', que significa que su puntaje ROUND se suma al puntaje GLOBAL. En ese caso, sera el turno del proximo jugador.
+- El primer jugador en alcanzar los 100 puntos GLOBAL gana el juego.
+
+*/
