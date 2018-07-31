e1b2c306282fd93cb88b75a013ae13a608bcf52d
Matias Scalella <matias.scalella@live.com>
2018-07-31 17:27:57 -0300

    inicializo archivo de javascript

--- app.js
+++ app.js
@@ -8,3 +8,33 @@ Reglas del juego:
 - El primer jugador en alcanzar los 100 puntos GLOBAL gana el juego.
 
 */
+
+var scores, roundScore, activePlayer, dice;
+
+scores = [0, 0]; // el primer valor corresponde al puntaje del primer jugador y el segundo, al segundo jugador.
+roundScore = 0;
+activePlayer = 0; // 0 es el primer jugador, 1 es el segundo jugador
+
+// Creo el dado del juego
+dice = Math.floor(Math.random() * 6) + 1; // genero un valor aleatorio entre 1 y 6 para el dado
+
+// Cambio el contenido del elemento HTML que suma puntos en funcion del jugador que esté activo
+document.querySelector('#current-' + activePlayer).textContent = dice;
+
+/*
+
+Otra forma de hacer lo mismo sería a través del método .innerHTML como se muestra abajo
+
+document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+
+Otra forma de seleccionar un elemento
+// Guardo el valor del texto en una variable que puedo utilizar luego
+
+let x = document.querySelector('#score-' + activePlayer).textContent; 
+console.log(x);
+
+*/
+
+
+// Selecciono la imagen del dado para que no se vea al comenzar el juego
+document.querySelector('.dice').style.display = 'none'; 
